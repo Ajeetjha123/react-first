9d3070850f85198fec2d9e4a71dd42ec46d6b8bc
Ajeet Kumar Jha <84560142+Ajeetjha123@users.noreply.github.com>
2023-06-16 18:12:01 +0530

    Add files via upload
    component file created

---
+++ ExpenceItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h3>Expenses Item</h3>
+      <p>Food Rs 10</p>
+      <p>Petrol Rs 100</p>
+      <p>Movies Rs 300</p>
+    </div>
+  );
+}
+
+export default ExpenseItem;
